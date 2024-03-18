c231e381ab0487251fe500e71dc5864c5abc208e
rois90 <roissm90@gmail.com>
2024-03-18 13:06:07 +0100

    add design model

--- src/api/design/designs.controllers.js
+++ src/api/design/designs.controllers.js
@@ -3,8 +3,29 @@ const { transporter } = require('../../utils/nodemailer-config');
 const User = require('../users/users.model');
 
 const create = async (req, res , next) => {
+    const userId = req.params;
+    const {nameDesign, elementType, children, grandSon, count, defaultStyles} = req.body;
+
+    if(!userId) {
+        return res.status(404).json({msg: 'ID de usuario no encontrado'})
+    }
+
     try {
         const newDesign = new Design;
+        res.status(200).json({msg: 'Diseño creado correctamente'});
+
+        newDesign.nameDesign = nameDesign;
+        newDesign.elementType = elementType;
+        newDesign.defaultContent.children = children;
+        newDesign.defaultContent.grandSon = grandSon;
+        newDesign.defaultContent.count = count;
+        newDesign.defaultStyles = defaultStyles;
+        newDesign.save()
+        res.status(200).json({msg: 'Diseño guardado correctamente', data: newDesign});
+
+        const user = await User.findById(userId);
+        user.designs = newDesign._id;
+
     }catch (error){
         return next(error);
     }
@@ -20,6 +41,7 @@ const remove = async (req, res , next) => {
         return next(error);
     }
 };
+
 const edit = async (req, res, next) => {
     const designId = req.params;
     const html = req.body.html;
--- src/api/design/designs.model.js
+++ src/api/design/designs.model.js
@@ -1,105 +1,63 @@
 const mongoose = require('mongoose');
 
-const designSchema = new mongoose.Schema({
+const DesignSchema = new mongoose.Schema({
   nameDesign: {
     type: String,
     required: true,
     trim: true 
   },
-  packs:{
-    name: {
-        type:String,
-        required:true,
-    },
-    html: {
-        type:String,
-        required:true,
-    },  
-//     css: {
-//         required:true ,
-//         properties:{ 
-//             width:{
-//                 number:{
-//                     type:Number,
-//                 },
-//                 type:{
-//                     type:String,
-//                     enum:["%","px"],
-//                 }
-//             },
-//             heigth:{
-//                 number:{
-//                     type:Number,
-//                 },
-//                 type:{
-//                     type:String,
-//                     enum:["%","px"],
-//                 }
-//             },
-//             display: { type: String, default: "flex" },
-//             flex_direction: { type: String, enum: ["row", "row-reverse", "column", "column-reverse"], required: true },
-//             flex_wrap: { type: String, enum: ["wrap", "nowrap"], required: true },
-//             justify_content: { type: String, enum: ["flex-start", "flex-end", "center", "space-between", "space-around", "space-evenly"], required: true },
-//             align_items: { type: String, enum: ["flex-start", "flex-end", "center", "baseline", "stretch"], required: true }, // Ajustado para incluir valores válidos de align-items
-//             position: { type: String, enum: ["absolute", "relative", "fixed", "sticky"], required: true }, // Corregido "absolte" por "absolute"
-//             top:"",
-//             bottom:"",
-//             left:"",
-//             right:"",
-//             color: "",
-//             background_color:"",
-//             font_size:"",   
-//             font_weight:"",
-//             text_decoration:"none",
-//             border_radius:{
-//                 number:{
-//                     type:Number,
-//                 },
-//                 type:{
-//                     type:String,
-//                     enum:["%","px"],
-//                 }
-//             },
-//             border:{
-//                 number:{
-//                     type:Number,
-//                 },
-//                 size:{
-//                     type:String,
-//                     enum:["%","px"],
-//                 },
-//                 type:{
-//                     type:String,
-//                     enum:["solid","dotted","dashed","double","groove","ridge","inset","outset"],
-//                 },
-//                 color:{
-//                     type:String,
-//                 }
-
-//             },
-
-//             },
-//            className: {
-//             type:String,
-//             required:true,    
-//             }  
-// },
-    css:[{
-        type:String,
-        required:true,
-    }],
-    userId: {
+  elementType: {
+    type: String,
+    required: true,
+    trim: true 
+  },
+  defaultContent: {
+    children: [],
+    grandSon: [],
+    count: {
+      type: Number,
+      required: true
+    }
+  },
+  defaultStyles: [{
+    type: String,
+    required: true
+  }],
+  userId: {
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'users',
     required: true
   },
   tickets: [{
     type: mongoose.Schema.Types.ObjectId, 
-    ref: 'tickets'
-  }]
-}}
-);
+    ref: 'tickets',
+    default: null
+  }],
+  edit: {
+    text: [],
+    bgColorNav: null,
+    bgColorForm: null,
+    bgColorButton: null,
+    bgColorSection: null,
+    bgColorArticle: null,
+    bgColorFigure: null,
+    bgColorFooter: null,
+    bgColorDiv: null,
+    bgColorTable: null,
+    colorTitle: null,
+    colorItem: null,
+    colorText: null,
+    fontSizeTitle: null,
+    fontSizeItem: null,
+    fontSizeText: null,
+    textDecorationTitle: null,
+    textDecorationText: null,
+    fontWeightTitle: null,
+    fontWeightItem: null,
+    fontWeightText: null,
+  }
+});
 
-const Design = mongoose.model('designs', designSchema);
+const Design = mongoose.model('designs', DesignSchema);
 module.exports = Design;
 
