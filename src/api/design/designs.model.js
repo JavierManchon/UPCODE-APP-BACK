428ca53183d26c069386ec9a0df9952a282668dc
JavierManchon <javiermanchongarcia@gmail.com>
2024-03-20 12:00:58 +0100

    Fix: Design model types fixed

--- src/api/design/designs.model.js
+++ src/api/design/designs.model.js
@@ -40,32 +40,122 @@ const DesignSchema = new mongoose.Schema({
     default: null
   }],
   edit: {
-    text: null,
-    textItem: null,
-    textTitle: null,
-    bgColorNav: null,
-    bgColorForm: null,
-    bgColorButton: null,
-    bgColorSection: null,
-    bgColorArticle: null,
-    bgColorFigure: null,
-    bgColorFooter: null,
-    bgColorDiv: null,
-    bgColorTable: null,
-    colorTitle: null,
-    colorItem: null,
-    colorText: null,
-    fontSizeTitle: null,
-    fontSizeItem: null,
-    fontSizeText: null,
-    textDecorationTitle: null,
-    textDecorationText: null,
-    fontWeightTitle: null,
-    fontWeightItem: null,
-    fontWeightText: null,
-    border: null,
-    borderRadius: null,
-    outline: null
+    text: {
+      type: String,
+      default: null
+    },
+    textItem: {
+      type: String,
+      default: null
+    },
+    textTitle: {
+      type: String,
+      default: null
+    },
+    textArray: [{
+      type: String,
+      default: null
+    }],
+    textArray2: [{
+      type: String,
+      default: null
+    }],
+    textArrayBidimensional: [[{
+      type: String,
+      default: null
+    }]],
+    bgColorNav: {
+      type: String,
+      default: null
+    },
+    bgColorForm: {
+      type: String,
+      default: null
+    },
+    bgColorButton: {
+      type: String,
+      default: null
+    },
+    bgColorSection: {
+      type: String,
+      default: null
+    },
+    bgColorArticle: {
+      type: String,
+      default: null
+    },
+    bgColorFigure: {
+      type: String,
+      default: null
+    },
+    bgColorFooter: {
+      type: String,
+      default: null
+    },
+    bgColorDiv: {
+      type: String,
+      default: null
+    },
+    bgColorTable: {
+      type: String,
+      default: null
+    },
+    colorTitle: {
+      type: String,
+      default: null
+    },
+    colorItem: {
+      type: String,
+      default: null
+    },
+    colorText: {
+      type: String,
+      default: null
+    },
+    fontSizeTitle: {
+      type: String,
+      default: null
+    },
+    fontSizeItem: {
+      type: String,
+      default: null
+    },
+    fontSizeText: {
+      type: String,
+      default: null
+    },
+    textDecorationTitle: {
+      type: String,
+      default: null
+    },
+    textDecorationText: {
+      type: String,
+      default: null
+    },
+    fontWeightTitle: {
+      type: String,
+      default: null
+    },
+    fontWeightItem: {
+      type: String,
+      default: null
+    },
+    fontWeightText: {
+      type: String,
+      default: null
+    },
+    border: {
+      type: String,
+      default: null
+    },
+    borderRadius: {
+      type: String,
+      default: null
+    },
+    outline: {
+      type: String,
+      default: null
+    }
   }
 });
 
